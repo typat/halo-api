a5a1695b869593cc8cad70f69e1f2b42a17b57d1
Tyler Gregory <tyler.gregory@dealer.com>
2016-04-05 15:40:59 -0400

    : initial commit #2

---
+++ src/main/java/com/typat/HaloApiApplication.java
@@ -0,0 +1,12 @@
+package com.typat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaloApiApplication.class, args);
+	}
+}
